3c987e780023b22d8501ce2c417562a36e56f0d6
devnetnewbeb <37068522+devnetnewbeb@users.noreply.github.com>
2018-03-29 15:46:17 +0200

    Update ex18.py

--- ex18.py
+++ ex18.py
@@ -14,9 +14,12 @@ def print_two_again(arg1, arg2):
 
 
 def print_one(arg1):
-    print("I got nothin'.")
+    print(f"arg1: {arg1}")
 
-
+def print_none():
+   print("I got nothin'.")
+    
+    
 print_two("Zed", "Shaw")
 print_two_again("Zed", "Shaw")
 print_one("First!")
