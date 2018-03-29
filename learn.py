4d5ab8da71d3531ed564ea00dbfdd36e2dbde9ad
devnetnewbeb <37068522+devnetnewbeb@users.noreply.github.com>
2018-03-29 12:02:05 +0200

    Update learn.py

--- learn.py
+++ learn.py
@@ -1,23 +1,5 @@
 
 
-Skip to content
-This repository
-Search
-Pull requests
-Issues
-Marketplace
-Explore
- @devnetnewbeb
-Sign out
-0
-0 0 devnetnewbeb/python
- Code  Issues 0  Pull requests 0  Projects 0  Wiki  Insights  Settings
-python/ex16.py
-9671a54  39 minutes ago
-@devnetnewbeb devnetnewbeb Create ex16.py
-     
-36 lines (23 sloc)  667 Bytes
-
 from sys import argv
 
 script, filename = argv
@@ -52,16 +34,3 @@ target.write("\n")
 
 print("And finally, we close it.")
 target.close()
-© 2018 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-API
-Training
-Shop
-Blog
-About
-Press h to open a hovercard with more details.
