f9695d840f97c8fc1fe8bbb011c2c84877bd437c
devnetnewbeb <37068522+devnetnewbeb@users.noreply.github.com>
2018-03-30 17:45:23 +0200

    Update example2.py

--- example2.py
+++ example2.py
@@ -8,11 +8,11 @@ netconf_filter = open("filter.ietf-interface.xml").read()
 
 if __name__ == '__main__':
     with manager.connect(host=ios_xe1["address"], port=ios_xe1["port"],
-        username=ios_xe1["username"],
-        password=ios_xe1["password"],
-        hostkey_verify=False) as m:
+            username=ios_xe1["username"],
+            password=ios_xe1["password"],
+            hostkey_verify=False) as m:
 
-    netconf_reply = m.get(netconf_filter)
+        netconf_reply = m.get(netconf_filter)
 
     intf_details = xmltodict.parse(netconf_reply.xml)["rpc-reply"]["data"]
     intf_config = intf_details["interfaces"]["interface"]
