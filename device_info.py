ff163b41b5f5387710234e9f22f148cdec4b0cb8
devnetnewbeb <37068522+devnetnewbeb@users.noreply.github.com>
2018-03-30 16:19:22 +0200

    Create device_info.py

---
+++ device_info.py
@@ -0,0 +1,18 @@
+ios_xe1 = {
+              "address": "10.1.1.131",
+              "port": 10000,
+              "username": "cisco",
+              "password": "cisco"
+           }
+
+
+
+
+ios_xe2 = {
+
+              "address": "10.1.1.132",
+              "port": 830,
+              "username": "cisco",
+              "password": "cisco"
+
+           }
